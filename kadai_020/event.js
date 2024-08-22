dc4f398c89c8acd12246e4ea239831cf5b818648
syp.1209 <47263084+syp1209@users.noreply.github.com>
2024-08-22 18:17:42 +0900

    DOM操作を使用して文字表示を変えよう　課題提出
    課題を提出します。
    ご確認お願い致します。

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+  // output-btnというidを持つHTML要素を取得し、定数に代入する
+  const btn = document.getElementById('btn');
+  
+  // HTML要素がクリックされたときにイベント処理を実行する
+  btn.addEventListener('click', () => {
+    const txt = document.getElementById('text');
+    txt.textContent = 'ボタンをクリックしました';
+  });
