e0e446cb8a4dde42aa5c11c15e91e42d84e9cb4f
syp.1209 <47263084+syp1209@users.noreply.github.com>
2024-08-23 09:01:44 +0900

    非同期処理を使用して、2秒後に文章を書き換えよう　課題提出
    課題を提出します。
    ご確認をお願い致します。

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+  // btnというidを持つHTML要素を取得し、定数に代入する
+  const btn = document.getElementById('btn');
+
+  // HTML要素がクリックされたときにイベント処理を実行する
+  btn.addEventListener('click', () => {
+    // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+    setTimeout(() => {
+      const txt = document.getElementById('text');
+      txt.textContent = 'ボタンをクリックしました'
+    }, 2000);
+  });
+
